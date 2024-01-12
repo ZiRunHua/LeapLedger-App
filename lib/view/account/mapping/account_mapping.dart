e6c392d120004f092da751ecc87960b01c5ebd16
小滋润 <807046079@qq.com>
2024-01-12 19:28:48 +0800

    fix

---
+++ lib/view/account/mapping/account_mapping.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AccountMapping extends StatefulWidget {
+  const AccountMapping({super.key});
+
+  @override
+  State<AccountMapping> createState() => _AccountMappingState();
+}
+
+class _AccountMappingState extends State<AccountMapping> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
