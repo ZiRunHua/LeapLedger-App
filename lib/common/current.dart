04ffbf6771a1ee7b7c321c7ceb679f1c26df43a5
小滋润 <807046079@qq.com>
2024-09-18 21:50:54 +0800

    changes

--- lib/common/current.dart
+++ lib/common/current.dart
@@ -1,26 +1,17 @@
 import 'dart:io';
 
-import 'package:leap_ledger_app/common/global.dart';
 import 'package:device_info_plus/device_info_plus.dart';
 
 enum ENV { dev, release }
 
 class Current {
-  static const String cacheKey = 'current';
-  static late String token;
-  static late int accountId;
   static late String peratingSystem; //android ios window
   static late final String? deviceId;
   static init() async {
     _initDeviceId();
 
     peratingSystem = Platform.operatingSystem;
-    Map<String, dynamic> prefsData = Global.cache.getData(cacheKey);
-    token = '';
-    accountId = prefsData['accountId'] ?? 0;
   }
-
-  static saveToCache() => Global.cache.save(cacheKey, {'token': token, 'accountId': accountId});
 
   static Future<void> _initDeviceId() async {
     var deviceInfo = DeviceInfoPlugin();
